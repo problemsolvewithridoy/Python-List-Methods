e509f424fe9a0804963df1c6e0b056723ee538af
Md Ridoy Hossain <inception.helps@gmail.com>
2023-04-26 10:26:14 +0600

    append function

---
+++ main.py
@@ -0,0 +1,19 @@
+from package import do_logging as do
+
+
+class listfile:
+    try:
+        def __init__(self,l):
+            self.l=l
+            do.log("this data geven by user: %s ", self.l)
+
+        def do_append(self,value):
+            try:
+                do.log("this is new data whcich append by user: %s ",value)
+                self.l[len(self.l):len(self.l)] = [value]
+                do.log("this is a final list after append data: %s ", self.l)
+
+
+            except Exception as e:
+                do.log_error("Error is happend")
+                do.log_exception("error is: %s", str(e))
